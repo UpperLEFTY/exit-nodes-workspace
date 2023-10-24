82ee6224ed499c6cbfd47b16f8d1985716ebaf98
Essence Alexander <upperlefty@zoho.eu>
2023-10-24 00:23:40 -0700

    footer component wip

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { makeStyles } from '@mui/material/styles';
+
+const useStyles = makeStyles(() => ({
+  footer: {
+    backgroundColor: '#000000',
+    color: '#ffffff',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px',
+    '&.dark': {
+      backgroundColor: '#000000',
+      color: '#ffffff'
+    }
+  }
+}));
+
+const Footer = () => {
+  const classes = useStyles();
+
+  return (
+    <footer className={`${classes.footer} footer`}>
+      <div className='container'>
+        <div>
+          <a href='mailto:upperlefty@zoho.eu?Subject=Hey,%20Essence!&body=Just%20wanted%20to%20reach%20out%20and...' aria-label='Email upperlefty'>
+            upperlefty@zoho.eu
+          </a>
+        </div>
+        <p>&copy; 2023 Exit-Nodes. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
