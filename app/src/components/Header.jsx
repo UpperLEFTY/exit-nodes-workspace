324733f6dcfc59950527df1ca08457d329604d04
Essence Alexander <upperlefty@zoho.eu>
2023-10-23 23:43:35 -0700

    created header component added styling

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,9 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
 import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+    '&.dark': {
+      backgroundColor: theme.palette.primary.dark,
+      color: theme.palette.primary.contrastText
+    }
+  }
+}));
 
 const Header = ({ darkMode, setDarkMode }) => {
   const [blogTitle, setBlogTitle] = useState('');
+  const classes = useStyles();
 
   useEffect(() => {
     axios.get('/api/blog-title')
@@ -16,7 +34,7 @@ const Header = ({ darkMode, setDarkMode }) => {
   }, []);
 
   return (
-    <header className={`App-header ${darkMode ? 'dark' : ''}`}>
+    <header className={`${classes.header} ${darkMode ? 'dark' : ''}`}>
       <h1>
         {blogTitle}
       </h1>
